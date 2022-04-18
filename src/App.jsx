706dd44f82c1b5eb125506c25728dbc0e2613a57
Ea Francisco <emberkasai04@gmail.com>
2022-04-18 10:39:44 -0400

    create App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
     return(
-        <Router>
-
-        </Router>
+        <>
+        <h1>Hello</h1>
+        </>
 
     )
 }
 
-export default App;
+export default App; 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/style.css';
 import App from './App';
+import './css/style.css';
+
 
 
 ReactDOM.render(
