dec35c233ec370311e62362571531a6b3414aedf
Ea Francisco <emberkasai04@gmail.com>
2022-04-20 23:32:33 -0400

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Pages from './pages/Pages';
 import Category from './components/Category';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { SiCodechef } from 'react-icons/si';
 
 function App() {
     return(
         <div className="App">
             <Router>
+                <Nav>
+                    <Logo to="/">
+                        <SiCodechef size={70}/> Code Cooker
+                    </Logo>
+                </Nav>
+
                 <Category />
                 <Pages />
             </Router>
@@ -13,5 +21,24 @@ function App() {
 
     )
 }
+
+const Logo = styled(Link)`
+    text-decoration: none;
+    font-size: 1.5rem;
+    display: flex;
+    align-items: center;
+`
+
+const Nav = styled.div`
+    padding: 3rem 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    svg {
+        padding: 1rem;
+    }
+`
+
 
 export default App; 
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,19 +1,40 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
+import { FaPizzaSlice } from 'react-icons/fa';
+import { GiCupcake, GiNoodles, GiBokChoy, GiTacos, GiButterToast, GiKnifeFork } from 'react-icons/gi';
 
 
 function Category() {
     return(
         <List>
+            <NavLink to="/cuisine/dessert">
+                <GiCupcake />
+                <h4>Dessert</h4>
+            </NavLink>
+
+            <NavLink to="/cuisine/asian">
+                <GiNoodles />
+                <h4>East Asian</h4>
+            </NavLink>
+
+            <NavLink to="/cuisine/mexican">
+                <GiTacos />
+                <h4>Mexican</h4>
+            </NavLink>
+
             <NavLink to="/cuisine/italian">
                 <FaPizzaSlice />
                 <h4>Italian</h4>
             </NavLink>
 
-            <NavLink to="/cuisine/american">
-                <FaHamburger />
-                <h4>hamburgers</h4>
+            <NavLink to="/cuisine/vegetarian">
+                <GiBokChoy />
+                <h4>Vegetarian</h4>
+            </NavLink>
+
+            <NavLink to="/cuisine/breakfast">
+                <GiButterToast />
+                <h4>Breakfast</h4>
             </NavLink>
         </List>
     )
